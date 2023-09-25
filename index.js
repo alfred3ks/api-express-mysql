e1ed42aaaa3cab181c3bdde07a456edfe02f28ab
Alfredo Sánchez <alfred3ks@gmail.com>
2023-09-26 01:28:51 +0200

    Defino las rutas básicas que tendra la app.

--- index.js
+++ index.js
@@ -2,6 +2,24 @@ import express from 'express';
 
 const app = express();
 
+// Definimos las rutas de la app:
+app.get('/', (req, res, next) => {
+  res.send('Home app');
+});
+
+app.get('/empleados', (req, res, next) => {
+  res.send('Solicitando empleados.');
+});
+app.post('/empleados', (req, res, next) => {
+  res.send('Creando empleados.');
+});
+app.put('/empleados', (req, res, next) => {
+  res.send('Actualizando empleados');
+});
+app.delete('/empleados', (req, res, next) => {
+  res.send('Borrando empleados.');
+});
+
 // Arrancamos el servidor:
 const PORT = 3000;
 app.listen(PORT, () => {
